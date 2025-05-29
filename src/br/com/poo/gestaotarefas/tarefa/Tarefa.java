c7d27f746513bca8c35584463760faacdd878b51
unknown <vmussio29@gmail.com>
2025-05-28 22:27:24 -0300

    Inicialização do repositório com o projeto de tarefas em java

---
+++ src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
@@ -0,0 +1,5 @@
+package br.com.poo.gestaotarefas.tarefa;
+
+public class Tarefa {
+
+}
---
+++ src/br/com/poo/gestaotarefas/teste/TesteTarefa.java
@@ -0,0 +1,7 @@
+package br.com.poo.gestaotarefas.teste;
+
+public class TesteTarefa {
+    public static void  main (String[] args){
+
+    }
+}
