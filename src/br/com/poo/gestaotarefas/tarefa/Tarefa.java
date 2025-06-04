a5118781757268abaaaea12edfd72da19a7abab1
victorzera02 <vmussio29@gmail.com>
2025-06-03 22:25:23 -0300

    adicionado atributos na classe tarefa

--- src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
+++ src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
@@ -1,5 +1,13 @@
 package br.com.poo.gestaotarefas.tarefa;
 
 public class Tarefa {
+    private int id;
+    private String descricao;
+    private boolean concluida;
 
+    public Tarefa(int id, String descricao, boolean concluida) {
+        this.id = id;
+        this.descricao = descricao;
+        this.concluida = concluida;
+    }
 }
