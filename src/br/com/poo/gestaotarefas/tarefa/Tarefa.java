ac77b071371ba8f3578781d69d3920b77af01df4
victorzera02 <vmussio29@gmail.com>
2025-06-03 22:47:07 -0300

    implementado exibição de detalhes da tarefe

--- src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
+++ src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
@@ -1,13 +1,17 @@
 package br.com.poo.gestaotarefas.tarefa;
 
-public class Tarefa {
+public class Tarefa{
     private int id;
     private String descricao;
     private boolean concluida;
 
-    public Tarefa(int id, String descricao, boolean concluida) {
+    public Tarefa(int id, String descricao, boolean concluida){
         this.id = id;
         this.descricao = descricao;
         this.concluida = concluida;
+    }
+
+    public String obterDados(){
+        return String.format("ID: %d | Descrição: %s | Status: %s", this.id, this.descricao, this.concluida ? "Concluída": "Em aberto");
     }
 }
--- src/br/com/poo/gestaotarefas/teste/TesteTarefa.java
+++ src/br/com/poo/gestaotarefas/teste/TesteTarefa.java
@@ -1,7 +1,11 @@
 package br.com.poo.gestaotarefas.teste;
 
+import br.com.poo.gestaotarefas.tarefa.Tarefa;
+
 public class TesteTarefa {
     public static void  main (String[] args){
+        Tarefa tarefa = new Tarefa(1, "Entrega 2 do PID", false);
 
+        System.out.println(tarefa.obterDados());
     }
 }
